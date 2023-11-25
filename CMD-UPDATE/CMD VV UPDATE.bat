1262424fc5305e8a8b2cf143722eb428a501d6ae
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-25 16:50:04 +0700

    Update CMD VV UPDATE.bat

--- CMD-UPDATE/CMD VV UPDATE.bat
+++ CMD-UPDATE/CMD VV UPDATE.bat
@@ -1,11 +1,11 @@
 :update1
 @echo off
 color a 
-del SLINKY
+del SLINKY 1.2.bat
 git clone https://github.com/rraapt66/SLINKY.git
 echo update Successful
 ping -n 2 127.0.0.1>nul
-del SLINKY
+del SLINKY 1.2.bat
 git clone https://github.com/rraapt66/SLINKY.git
 goto start
 :start
